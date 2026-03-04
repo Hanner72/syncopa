c041aaea7e732ca8f3ee00751e6b5f75195ab7c2
Johann Danner <johann.danner@gmail.com>
2026-03-04 08:23:10 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,7 @@
 /**
  * SYNCOPA - Musikvereinsverwaltung
  * Konfigurationsdatei
- * 
- * @version 2.2.1
+ * Copyright (c) 2026 by Johann Danner
  */
 
 // ============================================================================
